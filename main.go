66550219d5119ae457179b1aef5d5821382fbe6b
Kevin Shi <kevin@devze.ro>
2022-08-10 22:42:50 -0700

    init commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	} else {
+		fmt.Println("Loaded .envFile")
+	}
+}
